4b29e2a07865253437254c7a9bae27a24f35a8b4
Satwik Munshi <smunshi@uwaterloo.ca>
2018-05-02 00:01:11 -0400

    Initial Commit

---
+++ StarWarsInformation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWarsInformation
+//
+//  Created by Satwik Munshi on 2018-05-02.
+//  Copyright © 2018 Satwik Munshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
