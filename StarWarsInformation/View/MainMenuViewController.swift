70f325c50513696ca36e2a250185e9772abe0e8a
Satwik Munshi <smunshi@uwaterloo.ca>
2018-05-04 00:57:49 -0400

    Added super rough main menu layout along with gesture recognition

--- StarWarsInformation/View/MainMenuViewController.swift
+++ StarWarsInformation/View/MainMenuViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class MainMenuViewController: UIViewController {
-
+    
+    // Outlet connections
+    @IBOutlet weak var planetButton: UIImageView!
+    @IBOutlet weak var speciesButton: UIImageView!
+    @IBOutlet weak var characterButton: UIImageView!
+    @IBOutlet weak var vehicleButton: UIImageView!
+    
+    // Controller layer
+    let infoClient : InformationClient = InformationClient()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initializeImageTouchEvents()
     }
-
-
+    
+    func initializeImageTouchEvents() {
+        let planetTap = UITapGestureRecognizer(target: self, action: #selector(MainMenuViewController.handlePlanetButtonTapped))
+        planetButton.isUserInteractionEnabled = true
+        planetButton.addGestureRecognizer(planetTap)
+        
+        let speciesTap = UITapGestureRecognizer(target: self, action: #selector(MainMenuViewController.handleSpeciesButtonTapped))
+        speciesButton.isUserInteractionEnabled = true
+        speciesButton.addGestureRecognizer(speciesTap)
+        
+        let characterTap = UITapGestureRecognizer(target: self, action: #selector(MainMenuViewController.handleCharacterButtonTapped))
+        characterButton.isUserInteractionEnabled = true
+        characterButton.addGestureRecognizer(characterTap)
+        
+        let vehicleTap = UITapGestureRecognizer(target: self, action: #selector(MainMenuViewController.handleVehicleButtonTapped))
+        vehicleButton.isUserInteractionEnabled = true
+        vehicleButton.addGestureRecognizer(vehicleTap)
+        
+    }
+    
+    @objc
+    func handleVehicleButtonTapped() {
+        print("Vehicle Button Tapped!")
+    }
+    
+    
+    @objc
+    func handlePlanetButtonTapped() {
+        print("Planet Button Tapped!")
+    }
+    
+    @objc
+    func handleSpeciesButtonTapped() {
+        print("Species Button Tapped!")
+    }
+    
+    @objc
+    func handleCharacterButtonTapped() {
+        print("Character Button Tapped!")
+    }
+    
 }
 
