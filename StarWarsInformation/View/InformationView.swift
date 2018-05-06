4419c282de562e638ed9697f56484d281379369b
Satwik Munshi <smunshi@uwaterloo.ca>
2018-05-06 00:34:09 -0400

    Touch ups to text views

--- StarWarsInformation/View/InformationView.swift
+++ StarWarsInformation/View/InformationView.swift
@@ -51,13 +51,13 @@ public class InformationView {
     }
     public func generatePlanetText() -> String {
         let planet: Planet = Variables.planet
-        let text = "\(planet.name) is a planet with a population of \(planet.population). It has a \(planet.climate) climate and an orbital period of \(planet.orbitalPeriod). Its diameter is \(planet.diamater) and its gravitational force is \(planet.gravity). The planet is primarily a \(planet.terrain)."
+        let text = "\(planet.name) is a planet with a population of \(planet.population). It has a \(planet.climate) climate and an orbital period of \(planet.orbitalPeriod) days. Its diameter is \(planet.diamater) kilometers and its gravitational force is \(planet.gravity). The planet is primarily made up of \(planet.terrain)."
         return text
     }
     
     public func generateVehiclesText() -> String {
         let vehicle: Vehicle = Variables.vehicle
-        let text = "\(vehicle.name) is a vehicle of type \(vehicle.type). It is manufactured by \(vehicle.manufacturer) and has a cargo capacity of \(vehicle.cargoCapacity). Its typical crew complement is \(vehicle.crew) and it can carry up to \(vehicle.passengers) passengers. It costs around \(vehicle.cost) and is \(vehicle.length) long."
+        let text = "\(vehicle.name) is a vehicle of type \(vehicle.type). It is manufactured by \(vehicle.manufacturer) and has a cargo capacity of \(vehicle.cargoCapacity). Its typical crew complement is \(vehicle.crew) and it can carry up to \(vehicle.passengers) passengers. It costs around \(vehicle.cost) and is \(vehicle.length) metres long."
         return text
     }
     
